950b19abee12e95ed83f464e0d4cd6c9b6be052d
Jeevendra <jeevendrasingh1711@gmail.com>
2023-11-02 23:27:43 +0530

    Initial commit

---
+++ LoginPage/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using LoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace LoginPage.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountDBEntities1 dc = new AccountDBEntities1();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login login)
+        {            
+            var user = dc.Logins.SingleOrDefault(u => u.Name == login.Name);
+            if (user != null)
+            {
+                if (user.Password == login.Password)
+                {
+                    return RedirectToAction("Success", "Account");
+                }
+                else
+                {
+                    Session["Msg"] = "Password Incorrect";
+                    return View(login);
+                }
+            }
+            return View(login);
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(Login login)
+        {
+            dc.Logins.Add(login);
+            dc.SaveChanges();
+
+            return RedirectToAction("Login");
+
+        }
+        public ActionResult Success()
+        {
+            var log = dc.Logins;
+            return View(log);
+        }
+        public ViewResult Edit(int Id)
+        {
+            var log = dc.Logins.Find(Id);
+            return View(log);
+        }
+        [HttpPost]
+        public ActionResult Update(Login login)
+        {
+            dc.Entry(login).State = EntityState.Modified;
+            dc.SaveChanges();
+            return RedirectToAction("Success", "Account");
+        }
+    }
+}
---
+++ LoginPage/Models/LoginViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginPage.Models
+{
+    public class LoginViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+                   ErrorMessage = "Entered phone format is not valid.")]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
